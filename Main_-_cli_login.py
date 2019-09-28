59f1dc7949909925264db86743ed2dfcd18a8063
James <50501825+Gobot1234@users.noreply.github.com>
2019-09-28 15:28:48 +0100

    Add files via upload

--- Main_-_cli_login.py
+++ Main_-_cli_login.py
@@ -8,7 +8,7 @@ import discord
 from discord.ext import commands
 
 import sys
-import logging, traceback
+import traceback
 import json
 import threading
 import asyncio
@@ -19,7 +19,6 @@ preferences = json.loads(open('Login details/preferences.json', 'r').read())
 command_prefix = preferences["Command Prefix"]
 login = json.loads(open('Login details/sensitive details.json', 'r').read())
 token = login["Bot Token"]
-
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(command_prefix), case_insensitive=True)
 bot.remove_command('help')
@@ -41,19 +40,18 @@ if __name__ == '__main__':
             traceback.print_exc()
 
 
-
 # loading --------------------------------------------------------------------------------------------------------------
 
 
 @bot.event
 async def on_ready():
-    activity = f'{bot.owner_name}\'s trades | {LoaderCog.__version__} | Command Prefix \"{bot.command_prefix}\"'
+    activity = f'{bot.owner_name}\'s trades | V{LoaderCog.__version__} | Command Prefix \"{bot.command_prefix}\"'
     activity = discord.Activity(name=activity, type=discord.ActivityType.watching)
     await bot.change_presence(activity=activity)
-    print("Bot is ready")
-    print('Logged in as', bot.user.name, 'Send this to Gobot1234 to add your bot to the server', bot.user.id,
-          'to use the custom emojis',
-          '\nThis is:', LoaderCog.__version__)
+    print('-' * 30)
+    print('\033[92m' + bot.user.name + ' is ready' + '\033[92m')
+    print(f'Send this id: ' + '\033[95m' + f'\"{bot.user.id}\"' + '\033[95m' + '\033[92m' + ' to Gobot1234 to add your bot to the server to use the custom emojis',
+          '\nThis is: ' + '\033[95m' + f'Version {LoaderCog.__version__}' + '\033[95m')
     bot.dsdone = 1
     await asyncio.sleep(30)
 
@@ -62,52 +60,53 @@ async def on_ready():
                                               '\nTo do that type in your 2FA code into the console.')
         await asyncio.sleep(60)
     await bot.get_user(bot.owner_id).send('I\'m online both Steam and Discord dealing with your Steam messages')
+    print('-' * 30)
 
 
 # threading ------------------------------------------------------------------------------------------------------------
 
 
 def discordside():
+    print('\033[95m' + '-' * 30 + '\033[95m')
+    print('\033[95m' + 'Discord is logging on' + '\033[95m')
     bot.run(token)
 
 
 def steamside():
     while 1:
         while bot.dsdone != 0:
-            logging.basicConfig(format="%(asctime)s | %(message)s", level=logging.INFO)
-            LOG = logging.getLogger()
             client = SteamClient()
-            client.set_credential_location("Login Details/")  # where to store sentry files and other stuff  
+            print('\033[95m' + '-' * 30 + '\033[95m')
+            print('\033[95m' + 'Steam is now logging on' + '\033[95m')
+            client.set_credential_location('Login Details/')  # where to store sentry files and other stuff  
 
-            @client.on("error")
+            @client.on('error')
             def handle_error(result):
-                LOG.info(f'Logon result: {repr(result)}')
+                print('\033[91m' + f'Logon result: {repr(result)}' + '\033[91m')
 
-            @client.on("connected")
+            @client.on('connected')
             def handle_connected():
-                LOG.info(f'Connected to: {client.current_server_addr}')
+                print('\033[92m' + f'Connected to: {client.current_server_addr}' + '\033[92m')
 
-            @client.on("reconnect")
+            @client.on('reconnect')
             def handle_reconnect(delay):
-                LOG.info(f'Reconnect in {delay}...')
+                print('\033[94m' + f'Reconnect in {delay}...' + '\033[94m')
 
-            @client.on("disconnected")
+            @client.on('disconnected')
             def handle_disconnect():
-                LOG.info("Disconnected.")
+                print('\033[93m' + 'Disconnected.' + '\033[93m')
 
                 if client.relogin_available:
-                    LOG.info("Reconnecting...")
-                    client.reconnect(maxdelay=60)
+                    print('Reconnecting...')
+                    client.reconnect(maxdelay=30)
 
-            @client.on("logged_on")
+            @client.on('logged_on')
             def handle_after_logon():
                 global logged_on
                 logged_on = 1
-                LOG.info('-' * 30)
-                LOG.info(f'Logged on as: {client.user.name}')
-                LOG.info('-' * 30)
+                print('\033[92m' + f'Logged on as: {client.user.name}' + '\033[92m')
 
-            @client.on("chat_message")
+            @client.on('chat_message')
             def handle_message(user, message_text):
                 if user.steam_id == bot.bot64id:
                     if 'view it here' not in message_text and 'marked as declined' in message_text:
@@ -119,14 +118,10 @@ def steamside():
                             bot.usermessage = bot.sbotresp
                         if bot.currenttime == '23:59' and "You've made" in bot.sbotresp:
                             bot.graphplots = bot.sbotresp
-                else:
-                    pass
 
             result = client.cli_login(username=bot.username, password=bot.password)
             if result != EResult.OK:
-                LOG.info(f'Failed to login: {repr(result)}')
-                sys.exit()
-
+                print('\033[91m' + f'Failed to login: {repr(result)}' + '\033[91m')
             while 1:
                 client.run_forever()
 
