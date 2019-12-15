bdb219052130440ae56681f25502f89b5f99d48d
James <50501825+Gobot1234@users.noreply.github.com>
2019-12-15 17:57:16 +0000

    V.1.3.3
    Added requirements.txt for install and premade the configs

---
+++ Login details/sensitive_details.py
@@ -0,0 +1,7 @@
+# Don't share these with anyone
+token = ''  # bot token - https://media.discordapp.net/attachments/381963689470984203/514834919021740076/no_client_secret.jpg
+username = ''  # steam user name
+password = ''  # steam password
+secrets = {  # delete this line
+    "identity_secret": '',
+    "shared_secret": ''}  # to this line if you don't have your secrets -
