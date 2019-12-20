8e30ddccac27102dc85557674d2050da9cec548c
James <50501825+Gobot1234@users.noreply.github.com>
2019-12-20 19:35:00 +0000

    Update preferences.py
    More stuff to come

--- Login details/preferences.py
+++ Login details/preferences.py
@@ -1,5 +1,6 @@
 # These can be shared freely
 bots_steam_id = 1234567890  # your bot's steam id this needs to be your 64 one
 command_prefix = '!'  # your command prefix can either be ! ? . or / as they are used by tf2automatic
-embed_colour = '2E3BAD'  # a hex colour cant have the # at the start eg. 2E3BAD
+cli_login = True  # change this to either True or False, based on if you have your steam secrets
+embed_colour = ''  # a hex colour cant have the # at the start eg. 2E3BAD
 path_to_temp = 'C:/Users/Nicklason/Desktop/all the code for the bots/temp'  # the path to your temp folder
