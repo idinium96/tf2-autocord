52772f5ca14cd2b6d76cfb35b0d45d1738ea2d41
James <50501825+Gobot1234@users.noreply.github.com>
2019-12-22 17:26:58 +0000

    V.1.4.1
    Changes:
    • Improved the logging output to make it not break
    • Fixed issues with pm2 not reopening the process
    • Fixed bugs with user messages not showing up (again)
    • Checking for updates is now run in  an executor
    • When using Automatic Login it will try to relogin one more time if the codes don't match

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from gevent.monkey import patch_socket, patch_ssl; patch_socket(); patch_ssl()
 from aiohttp import ClientSession
 from datetime import datetime
-from logging import getLogger, INFO, Formatter, FileHandler
+from logging import getLogger, Formatter, StreamHandler, FileHandler, DEBUG, ERROR
 from os import listdir
 from os.path import isfile, join
 from sys import stderr
 from traceback import print_exc
+from time import sleep
 
 from discord import ClientException
 from discord.ext.commands import Bot, when_mentioned_or, Cog
@@ -26,11 +27,18 @@ class AutoCord(Bot):
 
     async def setup(self, bot):
         bot.dsdone = False
-        bot.log = getLogger()
-        bot.log.setLevel(INFO)
-        handler = FileHandler(filename='tf2autocord.log', encoding='utf-8', mode='w')
-        handler.setFormatter(Formatter('%(asctime)s : %(levelname)s : %(name)s | %(message)s'))
-        bot.log.addHandler(handler)
+        # create logger with 'spam_application'
+        bot.log = getLogger('tf2autocord')
+        bot.log.setLevel(DEBUG)
+        fh = FileHandler(filename='tf2autocord.log', encoding='utf-8', mode='w')
+        fh.setLevel(DEBUG)
+        ch = StreamHandler()
+        ch.setLevel(ERROR)
+        formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+        bot.log.addHandler(fh)
+        bot.log.addHandler(ch)
 
         bot.initial_extensions = [f[:-3] for f in listdir('Cogs') if isfile(join('Cogs', f))]
         print(f'Extensions to be loaded are {bot.initial_extensions}')
@@ -53,20 +61,23 @@ class AutoCord(Bot):
 
         @bot.client.on('error')
         def handle_error(result):
-            bot.log.error('Logon result: %s', repr(result))
+            bot.log.error(f'Logon result: {repr(result)}')
 
         @bot.client.on('connected')
         def handle_connected():
-            bot.log.info('Connected to: %s', bot.client.current_server_addr)
+            bot.log.info(f'Connected to: {bot.client.current_server_addr}')
 
         @bot.client.on('reconnect')
         def handle_reconnect(delay):
-            bot.log.info('Reconnect in %s...', delay)
+            if bot.client is None:
+                raise SystemExit
+            bot.log.info(f'Reconnect in {delay}...')
 
         @bot.client.on('disconnected')
         def handle_disconnect():
             bot.log.warning('Disconnected.')
-
+            if bot.client is None:
+                raise SystemExit
             if bot.client.relogin_available:
                 bot.log.info('Reconnecting...')
                 bot.client.reconnect(maxdelay=30)
@@ -75,8 +86,7 @@ class AutoCord(Bot):
         def handle_after_logon():
             bot.s_bot = bot.client.get_user(bot.bot64id)
             bot.logged_on = True
-            print(f'Logged on as: {bot.client.user.name}')
-            bot.log.info('Logged on as: %s', bot.client.user.name)
+            bot.log.info(f'Logged on as: {bot.client.user.name}')
 
         @bot.client.on('chat_message')
         def handle_message(user, message_text):
@@ -95,19 +105,16 @@ class AutoCord(Bot):
             bot.log.info('Using automatic')
             SA = guard.SteamAuthenticator(sensitive_details.secrets).get_code()
             result = bot.client.login(username=sensitive_details.username, password=sensitive_details.password, two_factor_code=SA)
+            if result == EResult.TwoFactorCodeMismatch:
+                sleep(2)
+                result = bot.client.login(username=sensitive_details.username, password=sensitive_details.password,
+                                          two_factor_code=SA)
+
         if result != EResult.OK:
             bot.log.fatal('Failed to login: %s', repr(result))
-            bot.session.close()
             raise SystemExit
-        while 1:
-            try:
-                bot.client.run_forever()
-            except KeyboardInterrupt:
-                bot.client.logout()
-                print('Logging out')
-                bot.log.info('Logging out')
-                bot.session.close()
-                raise SystemExit
+        bot.client.run_forever()
+
 
     def run(self, bot):
         print('Discord is logging on')
@@ -115,14 +122,17 @@ class AutoCord(Bot):
         bot.log.info('Discord is logging on')
         bot.log.info('-' * 30)
         try:
-            bot.loop.run_in_executor(None, self.steam_start, bot)
+            bot.steam = bot.loop.run_in_executor(None, self.steam_start, bot)
             bot.launch_time = datetime.utcnow()
             super().run(sensitive_details.token)
         except RuntimeError:
             bot.log.info('Logging out')
-            bot.session.close()
+            bot.client = None
+        except KeyboardInterrupt:
+            bot.log.info('Logging out')
+            bot.client = None
+        finally:
             raise SystemExit
-
 
 if __name__ == '__main__':
     bot = AutoCord()
