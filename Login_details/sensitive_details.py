b44a8c0c256a716bce7608fe2fe684b8ed096d15
gobot1234 <gobot1234yt@gmail.com>
2019-12-26 03:34:55 +0000

    Changes

--- Login_details/sensitive_details.py
+++
@@ -1,7 +0,0 @@
-# Don't share these with anyone
-token = 'MzYzODAwMzUxNjA3ODgxNzI5.XdFMbA.L4aGJskhuPtWx3yfK6xx8XygyT8'  # bot token from
-username = 'gobot12342'  # steam user name
-password = 'Jameshbgobot1234'  # steam password
-secrets = {  # delete this line
-    "identity_secret": "HBFdSqFhRY9Wdyo1dIOMsmKUFg=",
-    "shared_secret": "CHBFdSqFhRY9Wdyo1dIOMsmKUFg="}  # to this line if you don't have your secrets
