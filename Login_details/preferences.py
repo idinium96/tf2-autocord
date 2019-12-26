bc94600532d9d26b2b2aae0968f5b1cc723bdca3
gobot1234 <gobot1234yt@gmail.com>
2019-12-26 13:17:25 +0000

    1.4.1
    None given

--- Login_details/preferences.py
+++
@@ -1,6 +0,0 @@
-# These can be shared freely
-bots_steam_id = 76561198400794682  # your bot's steam id this needs to be your 64 one
-command_prefix = '!'  # your command prefix can either be ! ? . or / as they are used by tf2automatic
-cli_login = False  # change this to either True or False, based on if you have your steam secrets
-embed_colour = '2E3BAD'  # a hex colour cant have the # at the start eg. 2E3BAD
-path_to_temp = 'C:/Users/James/TF2Auto/temp'  # the path to your temp folder eg. C:/Users/Nicklason/Desktop/all the code for the bots/temp
