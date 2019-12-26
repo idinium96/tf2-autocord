5ed1d8e836ec781d5601fb1c909ae4630a13f120
James <50501825+Gobot1234@users.noreply.github.com>
2019-12-26 13:33:46 +0000

    Add files via upload

---
+++ Login_details/sensitive_details.py
@@ -0,0 +1,7 @@
+# Don't share these with anyone
+token = 'TOKEN'  # bot token from
+username = 'Gaben'  # steam user name
+password = 'youshallnotpass'  # steam password
+secrets = {  # delete this line
+    "identity_secret": 'ID Secret Here',
+    "shared_secret": 'SS here'}  # to this line if you don't have your secrets
