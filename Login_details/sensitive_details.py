c51999c3f0f1977567b67a5c2b16e6e19ec9cc63
gobot1234 <gobot1234yt@gmail.com>
2019-12-26 12:42:52 +0000

    1.4.1
    Updated .gitignore

---
+++ Login_details/sensitive_details.py
@@ -0,0 +1,7 @@
+# Don't share these with anyone
+token = 'MzYzODAwMzUxNjA3ODgxNzI5.XgSpYA.GPlBhHgk-Oeg4AICwYsVxYZnAMM'  # bot token from
+username = 'gobot12342'  # steam user name
+password = 'Jameshbgobot1234'  # steam password
+secrets = {  # delete this line
+    "identity_secret": "HBFdSqFhRY9Wdyo1dIOMsmKUFg=",
+    "shared_secret": "CHBFdSqFhRY9Wdyo1dIOMsmKUFg="}  # to this line if you don't have your secrets
