af0df247af35a71aee467f5bb1ca8e4255a69812
James <50501825+Gobot1234@users.noreply.github.com>
2020-01-17 23:11:30 +0000

    Delete sensitive_details.py

--- sensitive_details.py
+++
@@ -1,7 +0,0 @@
-# Don't share these with anyone
-token = 'MjM4NDk0NzU2NTIxMzc3Nzky.CunGFQ.wUILz7z6HoJzVeq6pyHPmVgQgV4'  # bot token https://media.discordapp.net/attachments/381963689470984203/514834919021740076/no_client_secret.jpg
-username = 'gaben'  # steam user name
-password = 'youshallnotpass'  # steam password
-secrets = {  # delete this line
-    "identity_secret": "9VW0iN0hZDZ2HtL5LQZTgsW_0UfyuCaqzw=",
-    "shared_secret": "pBn_cAfKqBBQnSuiJ0OV0AP07VuVkM0Pxis2="}  # to this line if you don't have your secrets
