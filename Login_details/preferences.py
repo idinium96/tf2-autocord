8b1ab4c347a4fbb51289e1c661d7268c56721d87
James <50501825+Gobot1234@users.noreply.github.com>
2020-01-17 23:10:22 +0000

    V.1.5.2
    Changes IDK

---
+++ Login_details/preferences.py
@@ -0,0 +1,7 @@
+# These can be shared freely
+bots_steam_id = 1234567890  # your bot's steam id this needs to be your 64 one
+command_prefix = '!'  # your command prefix can either be ! ? . or / as they are used by tf2automatic
+cli_login = False  # change this to either True or False, based on if you have your steam secrets
+embed_colour = 'FFFFFFF'  # a hex colour cant have the # at the start eg. 2E3BAD
+files = 'C:/Users/Nicklason/Desktop/all the code for the bots/temp'  # the path to your temp folder
+channel_id = None  # the id of the channel that you want trades sent to by default owner
