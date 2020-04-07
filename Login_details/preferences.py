1c12fb2ca26089a59398f71273c51397726fec27
idinium96 <idinium96@users.noreply.github.com>
2020-04-07 10:17:21 +0800

    update preferences for NezukoBot

--- Login_details/preferences.py
+++ Login_details/preferences.py
@@ -1,14 +1,16 @@
 # These can be shared freely
 
 # Your bot's steam id this needs to be your 64 one
-bots_steam_id = 76561198400794682
+bots_steam_id = 76561198034345222
 # Your command prefix can either be ! ? . or / as they are used by tf2automatic
 command_prefix = '!'
 # Change this to either True or False, based on if you have your steam secrets
 cli_login = False
 # A hex colour cant have the # at the start eg. 2E3BAD
-embed_colour = '2E3BAD'
+embed_colour = 'A100F1'
 # The path to your files folder eg. C:/Users/Nicklason/Desktop/all the code for the bots/files
-files = 'C:/Users/James/tf2-automatic/files'
+files = '/home/ubuntu/tf2bot/NezukoBot/files/idin30'
 # The id of the channel that you want trades sent to if it's None they will be sent to you via DM
-channel_id = None
+channel_id_live_trades = 666807591892025388
+channel_id_trades_statistic = 681141901835239451
+channel_id_offer_review = 696875947391057970
