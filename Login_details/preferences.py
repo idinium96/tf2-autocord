655d175b751537b7a3ca7ff6bc226e2b5e32cf5d
idinium96 <idinium96@users.noreply.github.com>
2020-04-07 09:54:18 +0800

    update preferences.py for FuminoBot

--- Login_details/preferences.py
+++ Login_details/preferences.py
@@ -1,14 +1,17 @@
 # These can be shared freely
 
 # Your bot's steam id this needs to be your 64 one
-bots_steam_id = 76561198400794682
+bots_steam_id = 76561198053911511
 # Your command prefix can either be ! ? . or / as they are used by tf2automatic
 command_prefix = '!'
 # Change this to either True or False, based on if you have your steam secrets
 cli_login = False
 # A hex colour cant have the # at the start eg. 2E3BAD
-embed_colour = '2E3BAD'
+embed_colour = '8BF329'
 # The path to your files folder eg. C:/Users/Nicklason/Desktop/all the code for the bots/files
-files = 'C:/Users/James/tf2-automatic/files'
+files = '/root/tf2bot/v3/files/theman_963'
 # The id of the channel that you want trades sent to if it's None they will be sent to you via DM
-channel_id = None
+channel_id_live_trades = 666807591892025388
+channel_id_trades_statistic = 681141901835239451
+channel_id_key_price = 686546469720817697
+channel_id_offer_review = 696875878298157087
