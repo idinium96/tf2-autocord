2bed88a80724182b0ca3ebd33e31e2534f195ae6
idinium96 <idinium96@users.noreply.github.com>
2020-04-10 00:03:31 +0800

    add user independent files as templates

---
+++ Login_details/template.preferences.py
@@ -0,0 +1,18 @@
+# These can be shared freely
+
+# Your bot's steam id this needs to be your 64 one
+bots_steam_id = 76561198400794682
+# Your command prefix can either be ! ? . or / as they are used by tf2automatic
+command_prefix = '!'
+# Your Discord user ID
+owner_id = 52786897960666666
+# Change this to either True or False, based on if you have your steam secrets
+cli_login = False
+# A hex colour cant have the # at the start eg. 2E3BAD
+embed_colour = '2E3BAD'
+# The path to your files folder eg. C:/Users/Nicklason/Desktop/all the code for the bots/files
+files = '/home/ubuntu/tf2bot/tf2-automatic/files/username'
+# The id of the channel that you want trades sent to if it's None they will be sent to you via DM
+channel_id_live_trades = None
+channel_id_trades_statistic = None
+channel_id_offer_review = None
---
+++ Login_details/template.sensitive_details.py
@@ -0,0 +1,12 @@
+# Don't share these with anyone
+
+# Bot token https://media.discordapp.net/attachments/381963689470984203/514834919021740076/no_client_secret.jpg
+token = 'MjM4NDk0NzU2NTIxMzc3Nzky.CunGFQ.wUILz7z6HoJzVeq6pyHPmVgQgV4'
+# Your Steam user name
+username = 'gaben'
+# Your Steam password
+password = 'youshallnotpass'
+# Your Steam secrets
+secrets = {  # delete all of this line 
+    "identity_secret": "9VW0iN0hZDZ2HtL5LQZTgsW_0UfyuCaqzw=",
+    "shared_secret": "pBn_cAfKqBBQnSuiJ0OV0AP07VuVkM0Pxis2="}  # to this line if you don't have your secrets
