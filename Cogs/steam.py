e997f851f4e2e1e3d6bc6797bdac863b071c3107
idinium96 <idinium96@users.noreply.github.com>
2020-04-11 07:50:59 +0800

    actual test

--- Cogs/steam.py
+++ Cogs/steam.py
@@ -125,7 +125,25 @@ class Steam(commands.Cog):
                     await self.bot.channel_offer_review.send(embed=embed)
                     await self.bot.channel_offer_review.send(f'<@!{ownerID}>, check this!')
 
-            elif sbotresp.startswith ('Other '):
+            elif sbotresp.startswith('Message from'):
+                embed = Embed(color=self.bot.color)
+                ownerID = preferences.owner_id
+                ids = findall(r'\d+', sbotresp)
+                trader_id = int(ids[0])
+                trader = self.bot.client.get_user(trader_id)
+                message = message.replace(f" {trader_id}", "")
+                if trader is not None:
+                    message = message.replace(f'Message from ','')
+                    embed.set_author(name=f'Message from: {trader.name}',
+                                     url=trader.steam_id.community_url,
+                                     icon_url=trader.get_avatar_url())
+                embed.description = message
+                embed.set_footer(text=f'Steam ID: {trader_id} • {datetime.now().strftime("%c")} UTC',
+                                 icon_url=self.bot.user.avatar_url)
+                await self.bot.channel_message.send(embed=embed)
+                await self.bot.channel_message.send(f'<@!{ownerID}>, check this!')
+
+            elif sbotresp.startswith ('Other admins'):
                 if 'sent a message' in sbotresp:
                     embed = Embed(color=self.bot.color, title='Message system info', description=sbotresp)
                     embed.set_footer(text=f'• {datetime.now().strftime("%c")} UTC', icon_url=self.bot.user.avatar_url)
