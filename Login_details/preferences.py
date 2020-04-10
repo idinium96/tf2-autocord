3a8b53f6cfc61c9796e4d1fa4d1c7b147f0ce30b
idinium96 <idinium96@users.noreply.github.com>
2020-04-11 00:41:45 +0800

    change IDs for test channels

--- Login_details/preferences.py
+++ Login_details/preferences.py
@@ -1,7 +1,7 @@
 # These can be shared freely
 
 # Your bot's steam id this needs to be your 64 one
-bots_steam_id = 76561198053911511
+bots_steam_id = 76561198163439235
 # Your command prefix can either be ! ? . or / as they are used by tf2automatic
 command_prefix = '!'
 # Your Discord user ID
@@ -9,13 +9,14 @@ owner_id = 527868979600031765
 # Change this to either True or False, based on if you have your steam secrets
 cli_login = False
 # A hex colour cant have the # at the start eg. 2E3BAD
-embed_colour = '8BF329'
+embed_colour = '2E3BAD'
 # The path to your files folder eg. C:/Users/Nicklason/Desktop/all the code for the bots/files
-files = '/home/ubuntu/tf2bot/FuminoBot/files/theman_963'
+files = '/home/ubuntu/tf2bot/TestBot/files/test'
 # The id of the channel that you want trades sent to if it's None they will be sent to you via DM
-channel_id_live_trades = 666807591892025388
-channel_id_trades_statistic = 681141901835239451
-channel_id_key_price = 686546469720817697
-channel_id_offer_review = 696875878298157087
-channel_id_polldata = 697107665075044453
-channel_id_pricelist = 697113570315337831
+channel_id_live_trades = 698205249776779354
+channel_id_trades_statistic = 698205401509789746
+channel_id_key_price = 698205519327920178
+channel_id_offer_review = 698200864208453642
+channel_id_polldata = 698205585249665165
+channel_id_pricelist = 698205703529037865
+channel_id_message = 698200940863291402
