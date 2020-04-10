bfaf84f35211e2e0e3996c4bfbcf350ce26f6e48
idinium96 <idinium96@users.noreply.github.com>
2020-04-11 01:26:25 +0800

    undo and fix typo

--- Cogs/steam.py
+++ Cogs/steam.py
@@ -143,7 +143,7 @@ class Steam(commands.Cog):
                 await self.bot.channel_message.send(embed=embed)
                 await self.bot.channel_message.send(f'<@!{ownerID}>, check this!')
 
-            elif 'send a message' in sbotresp:
+            elif 'sent a message' in sbotresp:
                 embed = Embed(color=self.bot.color, title='Message system info', description=sbotresp)
                 embed.set_footer(text=f'• {datetime.now().strftime("%c")} UTC', icon_url=self.bot.user.avatar_url)
                 await self.bot.channel_message.send(embed=embed)
