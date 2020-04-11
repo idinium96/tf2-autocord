32ccd05c25f3c4272437dc710f9241b471f4a2e2
idinium96 <idinium96@users.noreply.github.com>
2020-04-11 12:54:04 +0800

    add message feature

--- Login_details/preferences.py
+++ Login_details/preferences.py
@@ -13,9 +13,14 @@ embed_colour = '8BF329'
 # The path to your files folder eg. C:/Users/Nicklason/Desktop/all the code for the bots/files
 files = '/home/ubuntu/tf2bot/FuminoBot/files/theman_963'
 # The id of the channel that you want trades sent to if it's None they will be sent to you via DM
+#Constant every bot channels
 channel_id_live_trades = 666807591892025388
 channel_id_trades_statistic = 681141901835239451
-channel_id_key_price = 686546469720817697
+channel_id_message_ignore = 698389020656271390
+#FuminoBot Only
 channel_id_offer_review = 696875878298157087
+channel_id_message = 698189893829918761
 channel_id_polldata = 697107665075044453
 channel_id_pricelist = 697113570315337831
+channel_id_key_price = 686546469720817697
+
