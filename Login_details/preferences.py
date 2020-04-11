35f7499fcdfee1ea3b73f0ea635dfcb7f5b937cb
idinium96 <idinium96@users.noreply.github.com>
2020-04-11 12:34:21 +0800

    add note

--- Login_details/preferences.py
+++ Login_details/preferences.py
@@ -13,11 +13,13 @@ embed_colour = '2E3BAD'
 # The path to your files folder eg. C:/Users/Nicklason/Desktop/all the code for the bots/files
 files = '/home/ubuntu/tf2bot/TestBot/files/test'
 # The id of the channel that you want trades sent to if it's None they will be sent to you via DM
-channel_id_live_trades = 698205249776779354
-channel_id_trades_statistic = 698205401509789746
+# By bot
 channel_id_key_price = 698205519327920178
 channel_id_offer_review = 698200864208453642
 channel_id_polldata = 698205585249665165
 channel_id_pricelist = 698205703529037865
 channel_id_message = 698200940863291402
+#constant every bot
+channel_id_live_trades = 698205249776779354
+channel_id_trades_statistic = 698205401509789746
 channel_id_message_ignore = 698389020656271390
